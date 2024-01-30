ff17862477c4125bb1f3ac0c12a3cf871f9206c9
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2024-01-30 04:26:26 -0700

    Minor changes.

--- packages.el
+++ packages.el
@@ -17,6 +17,7 @@
 (package! tex-mode)
 (package! atomic-chrome)
 (package! yasnippet-snippets)
+(package! cape)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
